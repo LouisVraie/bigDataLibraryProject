d868012cdefb901c1777ff5dca5966846b5836bc
JulienLcq <158458481+JulienLcq@users.noreply.github.com>
2024-02-02 15:34:34 +0100

    update of the project, added Database.java

---
+++ src/main/java/scylla/Book.java
@@ -0,0 +1,18 @@
+package scylla;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.querybuilder.QueryBuilder;
+import com.datastax.oss.driver.api.querybuilder.insert.InsertInto;
+import com.datastax.oss.driver.api.querybuilder.select.Select;
+
+// For DML queries, such as SELECT
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.*;
+
+// For DDL queries, such as CREATE TABLE
+import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.*;
+
+public class Book {
+}
---
+++ src/main/java/scylla/Database.java
@@ -0,0 +1,24 @@
+package scylla;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.CqlSessionBuilder;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.querybuilder.select.Select;
+
+import java.net.InetSocketAddress;
+
+import static com.datastax.oss.driver.api.querybuilder.QueryBuilder.selectFrom;
+
+public class Database {
+    public String keyspace = "library";
+
+    public CqlSession getSession(){
+        return CqlSession.builder().withKeyspace(keyspace).build();
+    }
+    // Constructeur
+    public Database() {
+    }
+}
+
