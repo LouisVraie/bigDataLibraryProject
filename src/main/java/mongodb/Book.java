4f3e8d50be2ce81c24df0ec2c0b76794e4b50fa9
H4znow <haddouamine2002@gmail.com>
2024-02-04 17:52:12 +0100

    implemented book and reader

--- src/main/java/mongodb/Book.java
+++ src/main/java/mongodb/Book.java
@@ -1,25 +1,159 @@
 package mongodb;
 
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.model.*;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
-public class Book extends MongoDBCollection{
+public class Book extends MongoDBCollection {
 
-    Book(MongoDatabase database) {
+    public Book(MongoDatabase database) {
         super(database);
         this.collectionName = "book";
-        this.collection = database.getCollection(collectionName);
+        this.collection = database.getCollection(collectionName, Document.class);
     }
 
-    /* Other Méthods */
-    public void group_by_categories(Document filters){}  // group books by their category
-    public void copy_number(Document filters){} // print the number of copy for each book selected
-    public void worst_book_copies(Document filters){} // print the book which have the most copies of poor condition
-    public void available_copies(Document filters){} // print the number of copies available for selected book
-    public void most_read_author(Document whereQuery, int number){} // print the "number" most read author
-    public void loan_information(Document whereQuery){} // print information about loaned copies
-    public void loan_trends(Document whereQuery, int number){} // print the "number" most read book
+    public void group_by_categories(Document filters) {
+        Bson match = Aggregates.match(filters);
+        Bson unwind = Aggregates.unwind("$categories");
+        Bson group = Aggregates.group("$categories", Accumulators.sum("count", 1));
+        List<Bson> pipeline = Arrays.asList(match, unwind, group);
+
+        this.collection.aggregate(pipeline).forEach(printBlock);
+    }
+
+    public void copy_number(Document filters) {
+        Bson match = Aggregates.match(filters);
+        Bson unwind = Aggregates.unwind("$editions");
+        Bson unwindCopies = Aggregates.unwind("$editions.copies");
+        Bson group = Aggregates.group("$title", Accumulators.sum("totalCopies", 1));
+        List<Bson> pipeline = Arrays.asList(match, unwind, unwindCopies, group);
+
+        this.collection.aggregate(pipeline).forEach(printBlock);
+    }
+
+    public void worst_book_copies(Document filters) {
+        Bson match = Aggregates.match(filters);
+        Bson unwind = Aggregates.unwind("$editions");
+        Bson unwindCopies = Aggregates.unwind("$editions.copies");
+        Bson matchCondition = Aggregates.match(Filters.eq("editions.copies.wear", "Poor"));
+        Bson group = Aggregates.group("$title", Accumulators.sum("poorCopies", 1));
+        Bson sort = Aggregates.sort(Sorts.descending("poorCopies"));
+        Bson limit = Aggregates.limit(1);
+        List<Bson> pipeline = Arrays.asList(match, unwind, unwindCopies, matchCondition, group, sort, limit);
+
+        this.collection.aggregate(pipeline).forEach(printBlock);
+    }
+
+    public void available_copies(Document filters) {
+        Bson match = Aggregates.match(filters);
+        Bson unwind = Aggregates.unwind("$editions");
+        Bson unwindCopies = Aggregates.unwind("$editions.copies");
+        Bson matchAvailable = Aggregates.match(Filters.eq("editions.copies.state", true));
+        Bson group = Aggregates.group("$title", Accumulators.sum("availableCopies", 1));
+        List<Bson> pipeline = Arrays.asList(match, unwind, unwindCopies, matchAvailable, group);
+
+        this.collection.aggregate(pipeline).forEach(printBlock);
+    }
+
+    // Placeholder for printBlock, a Consumer<Document> to output the results of aggregations
+    private Consumer<Document> printBlock = document -> System.out.println(document.toJson());
+
+    // Methods requiring cross-collection data or more complex data relationships
+    // Conceptual approaches provided; actual implementation would depend on specific application logic and data model
+
+    public void most_read_author(int number) {
+        // Stage 1: Unwind the loans array from the readers collection
+        Bson unwindLoans = Aggregates.unwind("$loans");
+
+        // Stage 2: Group by the copy_id to count the number of times each copy has been loaned
+        Bson groupByCopyId = Aggregates.group("$loans.copy_id", Accumulators.sum("count", 1));
+
+        // Stage 3: Look up the corresponding book documents from the book collection based on copy_id
+        Bson lookupBooks = Aggregates.lookup("book", "_id", "editions.copies.copy_id", "book_docs");
+
+        // Stage 4: Unwind the resulting book_docs array
+        Bson unwindBooks = Aggregates.unwind("$book_docs");
+
+        // Stage 5: Unwind the authors array from the book documents
+        Bson unwindAuthors = Aggregates.unwind("$book_docs.authors");
+
+        // Stage 6: Group by author_id to count the total number of books loaned per author
+        Bson groupByAuthor = Aggregates.group("$book_docs.authors.id_author", Accumulators.sum("total_loans", "$count"));
+
+        // Stage 7: Sort the results by total_loans in descending order
+        Bson sortByTotalLoans = Aggregates.sortByCount("$total_loans");
+
+        // Stage 8: Limit the results to the specified number
+        Bson limitToTopN = Aggregates.limit(number);
+
+        // Combine all stages into a pipeline
+        List<Bson> pipeline = Arrays.asList(
+                unwindLoans,
+                groupByCopyId,
+                lookupBooks,
+                unwindBooks,
+                unwindAuthors,
+                groupByAuthor,
+                sortByTotalLoans,
+                limitToTopN
+        );
+
+        // Run the aggregation pipeline against the readers collection
+        MongoDatabase database = this.database;
+        database.getCollection("readers").aggregate(pipeline).forEach(printDocument());
+    }
+
+    // Helper method to print documents
+    private Consumer<Document> printDocument() {
+        return doc -> System.out.println(doc.toJson());
+    }
+
+    public void loan_information(Document whereQuery) {
+        // Assuming we're looking for loan information for specific criteria
+        Bson match = Aggregates.match(whereQuery);
+        List<Bson> pipeline = Arrays.asList(match);
+
+        // Assuming 'this.collection' is the 'readers' collection since it contains the loan data
+        this.collection.aggregate(pipeline).forEach(printDocument());
+    }
+
+    public void loan_trends(int number) {
+        // Stage 1: Unwind the loans array from the readers collection
+        Bson unwindLoans = Aggregates.unwind("$loans");
+
+        // Stage 2: Group by the book's copy_id to count the number of times each book has been loaned
+        Bson groupByCopyId = Aggregates.group("$loans.copy_id", Accumulators.sum("loanCount", 1));
+
+        // Stage 3: Sort by loanCount in descending order to find the most loaned books
+        Bson sortByLoanCount = Aggregates.sort(new Document("loanCount", -1));
+
+        // Stage 4: Limit the results to the top 'number' of loaned books
+        Bson limitToTopN = Aggregates.limit(number);
+
+        // Stage 5: Look up the corresponding book documents from the book collection based on copy_id
+        Bson lookupBooks = Aggregates.lookup("book", "_id", "editions.copies.copy_id", "book_docs");
+
+        // Stage 6: Unwind the resulting book_docs array (since lookup produces an array of matches)
+        Bson unwindBooks = Aggregates.unwind("$book_docs");
+
+        // Combine all stages into a pipeline
+        List<Bson> pipeline = Arrays.asList(
+                unwindLoans,
+                groupByCopyId,
+                sortByLoanCount,
+                limitToTopN,
+                lookupBooks,
+                unwindBooks
+        );
+
+        // Execute the pipeline against the 'readers' collection to get the loan trends
+        MongoDatabase database = this.database;
+        database.getCollection("readers").aggregate(pipeline).forEach(printDocument());
+    }
+
 }
--- src/main/java/mongodb/Reader.java
+++ src/main/java/mongodb/Reader.java
@@ -1,71 +1,72 @@
 package mongodb;
 
 import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.UpdateOptions;
-import com.mongodb.client.result.UpdateResult;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import mongodb.MongoDBCollection;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class Reader extends MongoDBCollection {
 
-    Reader(MongoDatabase database) {
+    public Reader(MongoDatabase database) {
         super(database);
         this.collectionName = "reader";
         this.collection = database.getCollection(collectionName);
     }
 
-    /* Other Méthods */
+    public void loaner_information(Document whereQuery) {
+        FindIterable<Document> readers = collection.find(whereQuery);
+        readers.forEach(printDocument());
+    }
 
-    /*
-    reader collection :
-    {
-        "id_reader": "reader_id",
-        "firstname": "Alice",
-        "lastname": "Smith",
-        "birth_date": "1980-02-02",
-        "street": "123 Library Street",
-        "city": "Booktown",
-        "postal_code": "12345",
-        "email": "alice.smith@email.com",
-        "phone_nb": "123-456-7890",
-        "loans": [
-            {
-            "loan_date": "2023-01-01",
-            "expiry_date": "2023-01-15",
-            "return_date": "",
-            "copy_id": "copy_id"
+    public void late_returns(Document whereQuery) {
+        Bson filter = Filters.and(
+                whereQuery,
+                Filters.lt("loans.expiry_date", new Date()),
+                Filters.eq("loans.return_date", "")
+        );
+
+        FindIterable<Document> readers = collection.find(filter);
+        readers.forEach(printDocument());
+    }
+
+    public void loan_count_by_day(Document filters) {
+        FindIterable<Document> readers = collection.find(filters);
+        Map<Date, Long> loanCountByDay = new HashMap<>();
+
+        for (Document reader : readers) {
+            List<Document> loans = reader.getList("loans", Document.class);
+            for (Document loan : loans) {
+                Date loanDate = loan.getDate("loan_date");
+                loanCountByDay.put(loanDate, loanCountByDay.getOrDefault(loanDate, 0L) + 1);
             }
-        ]
-    }
-    */
-
-
-    // print the current number of loan for each selected reader
-    public void loan_count_by_reader(Document filters){
-        System.out.println(" \n #### Loan count by reader ################################");
-        List<Document> documents = collection.find(filters).into(List.of());
-        Map<String, Long> loanCountByReader = documents.stream()
-                .flatMap(doc -> doc.getList("loans", Document.class).stream())
-                .filter(loan -> loan.getString("return_date").isEmpty())
-                .collect(Collectors.groupingBy(
-                        loan -> loan.getString("id_reader"),
-                        Collectors.counting()
-                ));
-
-        for (Map.Entry<String, Long> entry : loanCountByReader.entrySet()) {
-            System.out.println("Reader ID: " + entry.getKey() + ", Number of Loans with return_date = \"\": " + entry.getValue());
         }
+
+        loanCountByDay.forEach((date, count) ->
+                System.out.println("Date: " + date + ", Number of Loans: " + count));
     }
 
-    public void loaner_information(Document whereQuery){} // print information about reader who loaned book
-    public void late_returns(Document whereQuery){} // print the reader with the book that is in late
-    public void loan_count_by_day(Document filters){} // print the number of loan by date
-    public boolean can_loan_copies(String id_reader, int nb_wanted_loan){} // True if the reader can loan "nb_wanted_loan" copies
+    public boolean can_loan_copies(String id_reader, int nb_wanted_loan) {
+        Document reader = collection.find(new Document("id_reader", id_reader)).first();
+        if (reader == null) return false; // Reader not found
 
+        long currentLoans = reader.getList("loans", Document.class).stream()
+                .filter(loan -> loan.getString("return_date").isEmpty())
+                .count();
+
+        // Assume a business logic that a reader can have a maximum of 5 concurrent loans
+        int maxLoans = 5;
+        return (currentLoans + nb_wanted_loan) <= maxLoans;
+    }
+
+    // Helper method to print documents
+    private Consumer<Document> printDocument() {
+        return doc -> System.out.println(doc.toJson());
+    }
 }
